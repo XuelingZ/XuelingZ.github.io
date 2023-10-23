b660b3c975035fba5233dfa6753b1053c8d1f863
Vigneshwaran Ravichandran <rvigneshwaran.tr@gmail.com>
2023-10-23 13:23:04 -0400

    Added responsive design support

--- src/components/students.js
+++ src/components/students.js
@@ -1,52 +1,52 @@
 export default function Students() {
     return (
       <div>
-        <div class="wrapper"> 
+        <div className="wrapper"> 
     <h2>Students</h2>
 	
 
 <h3>Current Graduate Students</h3>
-<div class="stdcontainer">
+<div className="stdcontainer">
 	
-  <div class="stdtext">
+  <div className="stdtext">
 	  <ul> 
 		{/* <li>
-			<p class="stdp"><strong>Alan Romano</strong></p>
-			<p class="stdp"><strong>First-Author Publications:</strong> FSE&apos;23, WWW&apos;23, S&amp;P&apos;22, ASE&apos;21, ICSE&apos;21, ASE&apos;20, ASE&apos;20 Demo, ICSE&apos;20 Demo</p>
-			<p class="stdp"><strong>Awards:</strong> UB CSE Best Graduate Research Award (2021), Arthur A. Schomburg Fellowship (2018~2022)</p>
+			<p className="stdp"><strong>Alan Romano</strong></p>
+			<p className="stdp"><strong>First-Author Publications:</strong> FSE&apos;23, WWW&apos;23, S&amp;P&apos;22, ASE&apos;21, ICSE&apos;21, ASE&apos;20, ASE&apos;20 Demo, ICSE&apos;20 Demo</p>
+			<p className="stdp"><strong>Awards:</strong> UB CSE Best Graduate Research Award (2021), Arthur A. Schomburg Fellowship (2018~2022)</p>
 		</li>
 		<li>
-			<p class="stdp"><strong>Yutian Yan</strong></p>
-			<p class="stdp"><strong>First-Author Publications:</strong> ICSE&apos;23, IMC&apos;21</p>
+			<p className="stdp"><strong>Yutian Yan</strong></p>
+			<p className="stdp"><strong>First-Author Publications:</strong> ICSE&apos;23, IMC&apos;21</p>
 		</li> */}
 		<li>
-			<p class="stdp"><strong>Xiaoyi Yang</strong></p>
+			<p className="stdp"><strong>Xiaoyi Yang</strong></p>
 		</li>
 		<li>
-			<p class="stdp"><strong>Vigneshwaran Ravichandran</strong></p>
+			<p className="stdp"><strong>Vigneshwaran Ravichandran</strong></p>
 		</li>
 		<li>
-			<p class="stdp"><strong>Parinay Karande</strong></p>
+			<p className="stdp"><strong>Parinay Karande</strong></p>
 		</li>
 		
 		{/* <li>
-			<p class="stdp"><strong>Xiao Wu</strong></p>
+			<p className="stdp"><strong>Xiao Wu</strong></p>
 		</li> */}
 	  </ul>
   </div>
 </div>
 
 {/* <h3>Current Undergraduate Students</h3>
-<div class="stdcontainer">
+<div className="stdcontainer">
 	
-  <div class="stdtext">
+  <div className="stdtext">
 	  <ul> 
 		
 		<li>
-			<p class="stdp"><strong></strong></p>
+			<p className="stdp"><strong></strong></p>
 		</li>
 		<li>
-			<p class="stdp"><strong></strong></p>
+			<p className="stdp"><strong></strong></p>
 		</li>
 		
 	  </ul>
@@ -55,34 +55,34 @@ export default function Students() {
 
 	  
 {/* <h3>Current undergraduate students</h3>
-<div class="stdcontainer">
-  <div class="stdtext">
+<div className="stdcontainer">
+  <div className="stdtext">
 	  <ul>
-		  <li><p class="stdp"><strong>Junan Zhang</strong></p></li>
+		  <li><p className="stdp"><strong>Junan Zhang</strong></p></li>
 	  </ul>
   </div>
 </div>
 
 <h3>Graduate student alumni</h3>
-<div class="stdcontainer">
-  <div class="stdtext">
+<div className="stdcontainer">
+  <div className="stdtext">
 	  <ul> 
-		<li><p class="stdp"><strong>Xinyue Liu</strong> (PhD at UB)</p></li>
-		<li><p class="stdp"><strong>Charles Wiechec</strong> (PhD at UB)</p></li>
-		<li><p class="stdp"><strong>Ashish Avinash Naware</strong> (MS at UB, joined VMware)</p></li>
-		<li><p class="stdp"><strong>Ziang Li</strong> (MS at UB, joined Alibaba) </p></li>
-		<li><p class="stdp"><strong>Malav Dharmendrakumar Vyas</strong> (MS at UB, joined Palo Alto Networks)</p></li>
-		<li><p class="stdp"><strong>Yuchen Zhang</strong> (MS at UB, joined ByteDance)</p></li>
+		<li><p className="stdp"><strong>Xinyue Liu</strong> (PhD at UB)</p></li>
+		<li><p className="stdp"><strong>Charles Wiechec</strong> (PhD at UB)</p></li>
+		<li><p className="stdp"><strong>Ashish Avinash Naware</strong> (MS at UB, joined VMware)</p></li>
+		<li><p className="stdp"><strong>Ziang Li</strong> (MS at UB, joined Alibaba) </p></li>
+		<li><p className="stdp"><strong>Malav Dharmendrakumar Vyas</strong> (MS at UB, joined Palo Alto Networks)</p></li>
+		<li><p className="stdp"><strong>Yuchen Zhang</strong> (MS at UB, joined ByteDance)</p></li>
 	  </ul>
   </div>
 </div>
 
 <h3>Undergraduate student alumni</h3>
-<div class="stdcontainer">
-  <div class="stdtext">
+<div className="stdcontainer">
+  <div className="stdtext">
 	  <ul> 
-		<li><p class="stdp"><strong>Timothy Scholtz</strong> (undergraduate at UB)</p></li>
-		<li><p class="stdp"><strong>Nicholas Allen</strong> (undergraduate at Princeton)</p></li>
+		<li><p className="stdp"><strong>Timothy Scholtz</strong> (undergraduate at UB)</p></li>
+		<li><p className="stdp"><strong>Nicholas Allen</strong> (undergraduate at Princeton)</p></li>
 	  </ul>
   </div>
 </div>   */}
