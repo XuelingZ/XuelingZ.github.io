5f34cecfa6c952500eb0acdd85222bdc9b08871c
Xueling Zhang <30911899+XuelingZ@users.noreply.github.com>
2026-01-21 20:44:13 -0500

    Update teaching courses listed in teaching.js

--- src/components/teaching.js
+++ src/components/teaching.js
@@ -6,9 +6,9 @@ export default function Teaching() {
   <h3>Rochester Institute of Technology (Assistant Professor)</h3>
   <div className="notetext">
     <p className="homelist">
-      SWEN-262: Engineering of Software Subsystems <br />
-      SWEN-331: Engineering Secure Software<br />
-      SWEN-352: Software Testing <br />
+      SWEN-789: Engineering Privacy in Software <br />
+      SWEN-331: Engineering Secure Software <br />
+      SWEN-777: Software Quality Assurance <br />
       SWEN-561: Software Engineering Project I<br />
       SWEN-562: Software Engineering Project II <br />
     </p>
